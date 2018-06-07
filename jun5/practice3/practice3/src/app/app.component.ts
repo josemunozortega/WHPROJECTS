b86cc60f8733de4f5c131d3c01f4b623b6544171
Joey Ortega <j.ortega94@icloud.com>
2018-06-06 17:41:56 -0700

    jun6

---
+++ jun5/practice3/practice3/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+
+  class Person {
+    name: any;
+    gender: 'm', 'f';
+    rating: any;
+
+    constructor() {
+      vaR: male;
+      vaRR: female;
+    };
+  }
+}
